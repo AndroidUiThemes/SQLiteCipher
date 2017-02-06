7dca507d373bddc029d7e55baed7ecba0f4c3c08
Android Ui Themes <info@androiduithemes.com>
2017-02-07 00:41:18 +0530

    Implementation SQLite Cipher
    Implementation SQLite Cipher

--- app/src/main/java/app/SQLCipherExamples/MainActivity.java
+++ app/src/main/java/app/SQLCipherExamples/MainActivity.java
@@ -1,15 +1,25 @@
 package app.SQLCipherExamples;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView obj;
+    DBHelper mydb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,23 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mydb = new DBHelper(this);
+        ArrayList array_list = mydb.getAllCotacts();
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, array_list);
+
+        obj = (ListView) findViewById(R.id.listView1);
+        obj.setAdapter(arrayAdapter);
+        obj.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                int id_To_Search = arg2 + 1;
+                Bundle dataBundle = new Bundle();
+                dataBundle.putInt("id", id_To_Search);
+                Intent intent = new Intent(getApplicationContext(), DisplayContact.class);
+                intent.putExtras(dataBundle);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -29,16 +56,25 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        super.onOptionsItemSelected(item);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Bundle dataBundle = new Bundle();
+                dataBundle.putInt("id", 0);
+                Intent intent = new Intent(getApplicationContext(), DisplayContact.class);
+                intent.putExtras(dataBundle);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public boolean onKeyDown(int keycode, KeyEvent event) {
+        if (keycode == KeyEvent.KEYCODE_BACK) {
+            moveTaskToBack(true);
+        }
+        return super.onKeyDown(keycode, event);
     }
 }
